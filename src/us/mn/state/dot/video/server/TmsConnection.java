de8e734dc2e0ffe24a95f4925badac344b4adca4
Timothy Johnson <none@none>
2011-12-08 16:28:29 -0600

    removed anything unrelated to cameras from TmsConnection

--- src/us/mn/state/dot/video/server/TmsConnection.java
+++ src/us/mn/state/dot/video/server/TmsConnection.java
@@ -33,35 +33,14 @@ public class TmsConnection extends DatabaseConnection {
 	protected static final String CAMERA_PUBLISH = "publish";
 	protected static final String CAMERA_ENCODER_TYPE = "encoder_type";
 	
-	public static final int TYPE_CONTROLLER = 1;
-	public static final int TYPE_COMM_LINK = 2;
-	public static final int TYPE_CAMERA = 3;
-	public static final int TYPE_DETECTOR = 4;
-	public static final int TYPE_LCS = 5;
-	public static final int TYPE_DMS = 6;
-	public static final int TYPE_METER = 7;
-	public static final int TYPE_ROADWAY = 8;
-	
 	protected static final String TABLE_CAMERA = "camera_view";
-	protected static final String TABLE_DMS = "dms_view";
-	protected static final String TABLE_METER = "ramp_meter_view";
-	protected static final String TABLE_DETECTOR = "detector_view";
-	protected static final String TABLE_COMMLINK = "comm_link_view";
-	protected static final String TABLE_CONTROLLER = "controller_loc_view";
 
 	protected static final String F_CROSS_STREET = "cross_street";
 	protected static final String F_CROSS_DIR = "cross_dir";
 	protected static final String F_CROSS_MOD = "cross_mod";
 	protected static final String F_ROADWAY = "roadway";
 	protected static final String F_ROADWAY_DIR = "road_dir";
-
-	protected static final String F_DMS_ID = "name";
 	protected static final String F_CAMERA_ID = "name";
-	protected static final String F_METER_ID = "name";
-	protected static final String F_DETECTOR_ID = "det_id";
-	protected static final String F_COMMLINK_ID = "name";
-	protected static final String F_COMMLINK_URL = "url";
-	protected static final String F_CONTROLLER_ID = "name";
 	
 	private static final Hashtable<String, TmsConnection> connections =
 		new Hashtable<String, TmsConnection>();
@@ -182,38 +161,11 @@ public class TmsConnection extends DatabaseConnection {
 		return false;
 	}
 
-	/** Get the location of a device */
-	public String getLocation(int type, String deviceName){
-		String table = null;
-		String idField = null;
-		switch (type) {
-			case TYPE_DMS:
-				table = TABLE_DMS;
-				idField = F_DMS_ID;
-				break;
-			case TYPE_CAMERA:
-				table = TABLE_CAMERA;
-				idField = F_CAMERA_ID;
-				break;
-			case TYPE_DETECTOR:
-				table = TABLE_DETECTOR;
-				idField = F_DETECTOR_ID;
-				break;
-			case TYPE_METER:
-				table = TABLE_METER;
-				idField = F_METER_ID;
-				break;
-			case TYPE_CONTROLLER:
-				table = TABLE_CONTROLLER;
-				idField = F_CONTROLLER_ID;
-				break;
-			default:
-				break;
-		}
-		if(table == null || idField == null) return "";
+	/** Get the location of a camera */
+	public String getLocation(String name){
 		String q = "select " + F_ROADWAY + ", " + F_ROADWAY_DIR + ", " +
 			F_CROSS_STREET + ", " + F_CROSS_DIR + ", " + F_CROSS_MOD +
-			" from " + table + " where " + idField + " = '" + deviceName + "'";
+			" from " + TABLE_CAMERA + " where " + F_CAMERA_ID + " = '" + name + "'";
 		String loc = "";
 		try{
 			ResultSet rs = query(q);
@@ -230,100 +182,10 @@ public class TmsConnection extends DatabaseConnection {
 		return loc;
 	}
 
-	/** Get a list of names for a given device type
-	 */
-	public ArrayList<String> getNames(int type){
-		switch(type){
-			case(TYPE_CONTROLLER):
-				return getControllerNames();
-			case(TYPE_COMM_LINK):
-				return getCommLinkNames();
-			case(TYPE_CAMERA):
-				return getCameraNames();
-			case(TYPE_DETECTOR):
-				return getDetectorNames();
-			case(TYPE_DMS):
-				return getDMSNames();
-			case(TYPE_LCS):
-				return getLCSNames();
-			case(TYPE_METER):
-				return getMeterNames();
-			case(TYPE_ROADWAY):
-				return getRoadwayNames();
-		}
-		return new ArrayList<String>();
-	}
-	
-	private ArrayList<String> getControllerNames(){
-		ArrayList<String> list = new ArrayList<String>();
-		String sql = "select comm_link, drop_id " +
-				"from controller_loc_view ";
-		ResultSet set = query(sql);
-		if(set == null) return list;
-		try{
-			set.beforeFirst();
-			while(set.next()){
-				list.add(set.getString("comm_link") + "D" + set.getString("drop_id"));
-			}
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-		return list;
-	}
-	
-	/** Get a list of comm_link names. */
-	private ArrayList<String> getCommLinkNames(){
-		String sql = "select name from comm_link";
-		return getColumnList(sql, "name");
-	}
-
-	/** Get a list of meter names. */
-	private ArrayList<String> getMeterNames(){
-		String sql = "select id from ramp_meter_view";
-		return getColumnList(sql, "id");
-	}
-
-	/** Get a list of detector names. */
-	private ArrayList<String> getDetectorNames(){
-		String sql = "select det_id from detector_view";
-		return getColumnList(sql, "det_id");
-	}
-
-	/** Get a list of DMS names. */
-	private ArrayList<String> getDMSNames(){
-		String sql = "select id from dms_view";
-		return getColumnList(sql, "id");
-	}
-
 	/** Get a list of camera names. */
-	private ArrayList<String> getCameraNames(){
-		String sql = "select name from camera_view order by name";
-		return getColumnList(sql, "name");
-	}
-
-	/** Get a list of LCS names. */
-	private ArrayList<String> getLCSNames(){
-		String sql = "select id from lcs";
-		return getColumnList(sql, "id");
-	}
-
-	/** Get a list of Roadway names. */
-	private ArrayList<String> getRoadwayNames(){
-		String sql = "select name from road_view order by name";
-		return getColumnList(sql, "name");
-	}
-
-	/** Get the comm_link name for the given URL */
-	public String getCommLink(String url){
-		String sql = "select " + F_COMMLINK_ID + " from " + TABLE_COMMLINK +
-			" where " + F_COMMLINK_URL + " = '" + url + "'";
-		return getString(sql, F_COMMLINK_ID);
-	}
-
-	/** Get the url for the given comm_link name */
-	public String getURL(String name){
-		String sql = "select " + F_COMMLINK_URL + " from " + TABLE_COMMLINK +
-			" where " + F_COMMLINK_ID + " = '" + name + "'";
-		return getString(sql, F_COMMLINK_URL);
+	public List<String> getCameraNames(){
+		String sql = "select " + F_CAMERA_ID + " from " + TABLE_CAMERA +
+			" order by " + F_CAMERA_ID;
+		return getColumnList(sql, F_CAMERA_ID);
 	}
 }
